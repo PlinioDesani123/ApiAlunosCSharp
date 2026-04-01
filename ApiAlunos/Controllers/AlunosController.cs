a262a048e41f78710d82afd5793805a191b8d480
Plínio Desani <desaniplinio@gmail.com>
2026-03-31 21:00:19 -0300

    Prejeto API

---
+++ ApiAlunos/Controllers/AlunosController.cs
@@ -0,0 +1,54 @@
+﻿using ApiAlunos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAlunos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private List<Aluno> PegarDados()
+        {
+            // Criar uma lista de Alunos vazia para receber os dados do arquivo, desserializar o arquivo c:\temp\alunos.json para essa lista
+            List<Aluno> listaAlunos = new();
+            try
+            {
+                string dadosArquivo = System.IO.File.ReadAllText("c:\\temp\\alunos.json");
+                listaAlunos = System.Text.Json.JsonSerializer.Deserialize<List<Aluno>>(dadosArquivo);
+            }
+            catch
+            {
+                // Ignorar erros e retornar lista vazia
+            }
+            return listaAlunos;
+        }
+
+        [HttpGet]
+        public IEnumerable<Aluno> GetLista()
+        {
+            return PegarDados();
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarAluno(Aluno aluno)
+        {
+            List<Aluno> listaAlunos = PegarDados();
+
+            // Adicionar o novo aluno na lista de alunos 
+            listaAlunos.Add(aluno);
+
+            SalvarDados(listaAlunos);
+
+            return Ok();
+        }
+
+        private static void SalvarDados(List<Aluno> listaAlunos)
+        {
+            // Serializar a lista de alunos e salvar o arquivo c:\temp\alunos.json
+            string dadosSerializados = System.Text.Json.JsonSerializer.Serialize(listaAlunos);
+
+            // Salvar o arquivo c:\temp\alunos.json com os dados serializados    
+            System.IO.File.WriteAllText("c:\\temp\\alunos.json", dadosSerializados);
+        }
+    }
+}
---
+++ ApiAlunos/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiAlunos.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Celular { get; set; }
+        
+        public string Email { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+       
+
+    }
+}
